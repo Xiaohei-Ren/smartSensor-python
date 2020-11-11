fb7cf58d4bba6d4c0db932ce03f1183ab74e922e
Xiaohei-Ren <renyuke0010@163.com>
2020-11-11 09:28:25 +0800

    修改代码并增加注释

--- test/smartSensorDataRead.py
+++ test/smartSensorDataRead.py
@@ -7,7 +7,7 @@ import snap7
 def plc_connect(ip, type, rack=0, slot=1):
     """
     连接初始化
-    :param ip:
+    :param ip: plc中固定ip
     :param rack: 通常为0
     :param slot: 根据plc安装，一般为0或1
     :return:client
@@ -27,54 +27,56 @@ def plc_con_close(client):
     client.disconnect()
 
 
-def read_VB(client, offset):
-    """
-    :param client: client
-    :param offset: int
-    :returns: str.
-    """
-    vb_data = client.db_read(1, offset, 1)
-    return vb_data[0]
-
-
-def write_VB(client, offset, data):
-    """
-    :param client: client
-    :param offset: int
-    :param data: str
-    """
-    data = int(data)
-    temp = hex(int(data))[2:]
-    if data < 0 or data > 255:
-        print("请输入0-255之间的数")
-    else:
-        if data < 16:
-            temp = "0" + temp
-        client.db_write(1, offset, bytes.fromhex(temp))
-        print("向寄存器VB" + str(offset) + "写入" + str(data) + "成功")
-
-
 def sleep_time(hour, min, sec):
     return hour * 3600 + min * 60 + sec
 
 
-def con_DbInsert(host, pot, user, password, Db, id, temp, query):
+def read_VD(offset, count):
     """
+    读取VD中数据
+    :param offset: plc中起始位 例：VD100，VD200
+    :param count: 从起始位开始读取位数 VD数据通常为4位
+    :return: VDData
+    """
+    # 获取PLC中数据所在位VD200-VD204
+    offsetData = client_fd.read_area(0x84, 1, offset, count)
+    print(offsetData)
+    # 数据转换为浮点数1
+    VDData = snap7.util.get_real(offsetData, 0)
+    print(VDData)
+    # 数据转换为浮点数2
+    VDData1 = struct.unpack(">f", offsetData)
+    print(VDData1[0])
+    return VDData
+
+
+def con_DB(host, pot, user, password, Db):
+    """
+
     :param host: localhost
     :param pot: 8086
     :param user:
     :param password:
-    :param Db:
-    :param id: id
-    :param temp: 采集传入温度
-    :param query:'select * from test;'
+    :param Db: 数据库名
+    :return: InfluxClient
+    """
+    InfluxClient = InfluxDBClient(host, pot, user, password, Db)
+    clientList = InfluxClient.get_list_database()
+    print("connect success!")
+    print(clientList)
+    return InfluxClient
+
+
+def DB_Insert(InfluxClient, id, temp, query):
+    """
+    插入数据
+    :param InfluxClient:
+    :param id:
+    :param temp:温度
+    :param query:select语句
     :return:
     """
-    client = InfluxDBClient(host, pot, user, password, Db)
-    clientList = client.get_list_database
-    print(clientList)
-
-    # 写入数据库
+    # json形式写入
     tempJson = [{
         "measurement": 'test',
         "tags": {
@@ -84,30 +86,22 @@ def con_DbInsert(host, pot, user, password, Db, id, temp, query):
             'temp': temp,
         }
     }]
-    client.write_points(tempJson)
-    result = client.query(query)
+    InfluxClient.write_points(tempJson)
+    result = InfluxClient.query(query)
     print("Result: {0}".format(result))
 
 
 if __name__ == '__main__':
-    # 访问plc取数据
+    # 连接plc
     client_fd = plc_connect('192.168.2.1', 2)
     print("connect success")
 
     # 每5s读取温度数据并写入influxDb
     second = sleep_time(0, 0, 5)
     while True:
-        # 获取PLC中数据所在位VD200-VD204
-        data = client_fd.read_area(0x84, 1, 200, 4)
-        print(data)
-        # 数据转换为浮点数1
-        data1 = snap7.util.get_real(data, 0)
-        print(data1)
-        # 数据转换为浮点数2
-        data2 = struct.unpack(">f", data)
-        print("当前温度{}", data2[0])
+        # 从VD200开始读取4位，并转换为浮点数
+        data = read_VB(200, 4)
         time.sleep(second)
         # 数据存入influxDB（database：temdb，measurement：test）
-        con_DbInsert('localhost', 8086, 'root', '123456', 'temdb', 1, data1, 'select * from test;')
-
-    plc_con_close(client_fd)
+        conn = con_DB('localhost', 8086, 'root', '123456', 'temdb')
+        DB_Insert(conn, 1, data, 'select * from test;')
